073f49c75a39c60dab69a4adcf3489fe540bf15a
envorior <147531432+envorior@users.noreply.github.com>
2023-12-09 16:09:58 +0530

    Update publicappurls.py

--- publicapp/publicappurls.py
+++ publicapp/publicappurls.py
@@ -3,5 +3,12 @@ from .import views
 
 urlpatterns = [
     path('publichome/',views.publichome,name='publichome'),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
+    path('donation/',views.donation,name='donation'),
+    path('jobs/',views.jobs,name='jobs'),
+    path('policy/',views.policy,name='policy'),
+    path('scam/',views.scam,name='scam'),
+    path('terms/',views.terms,name='terms'),
     
 ]
