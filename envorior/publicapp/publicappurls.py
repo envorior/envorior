ebb84f53ea683bcf0bf06bae72282c4a6c4466bd
Muskan Maurya <muskan.maurya31@gmail.com>
2024-02-12 20:18:16 +0530

    new commit for servent zone

--- envorior/publicapp/publicappurls.py
+++ envorior/publicapp/publicappurls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:envorior/publicapp/publicappurls.py
 from django.urls import path
 from .import views
 
@@ -14,20 +13,4 @@ urlpatterns = [
     path('profile/',views.profile,name='profile'),
     
 ]
-=======
-from django.urls import path
-from .import views
 
-urlpatterns = [
-    path('publichome/',views.publichome,name='publichome'),
-    path('about/',views.about,name='about'),
-    path('contact/',views.contact,name='contact'),
-    path('donation/',views.donation,name='donation'),
-    path('jobs/',views.jobs,name='jobs'),
-    path('policy/',views.policy,name='policy'),
-    path('scam/',views.scam,name='scam'),
-    path('terms/',views.terms,name='terms'),
-    path('profile/',views.profile,name='profile'),
-    
-]
->>>>>>> dfec71a3c126fad87b8a1f3853527e4c5d2c0fcc:publicapp/publicappurls.py
