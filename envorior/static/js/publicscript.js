f3cc7bba3a4f7423e338417bde8307aa6854a812
anand gupta <anandgupta965193@gmail.com>
2024-02-23 21:40:50 +0530

    image of pages is fixed

--- envorior/static/js/publicscript.js
+++ envorior/static/js/publicscript.js
@@ -1,21 +1,53 @@
+var url ="http://127.0.0.1:8000"
+
 function other_settings() {
-    
-   popup();
+    let top="25%";
+    let left="50%";
+    let height="50vh";
+    let width="50vw";
+    let html=`<div><a href="{url}/login"
+      class="text-decoration-none"
+    >
+      <span class="txt-2"><i class="fa-solid fa-house"></i></span>
+      <span class="txt-2">Home</span>
+    </a>
+  </div>
+
+  <div>
+    <a
+      href="{%url 'publicapp:profile'%}"
+      class="text-decoration-none"
+    >
+      <span class="txt-2"><i class="fa-solid fa-house"></i></span>
+      <span class="txt-2">Profile</span>
+    </a>
+  </div>
+
+  <div>
+    <a href="{%url 'publicapp:donation'%}" class="text-decoration-none">
+      <span class="txt-2"
+        ><i class="fa-solid fa-hand-holding-dollar"></i
+      ></span>
+      <span class="txt-2">Donation</span>
+    </a>
+  </div>`;
+   popup(top,left,height,width,html);
     
 }
 
-function popup(){
+function popup(top, left, height, width,html){
     var popup = document.createElement("div");
-    popup.classList.add("popup"); // Use classList.add() to add a class
+    popup.classList.add("popup"); 
+    popup.innerHTML=html;// Use classList.add() to add a class
     document.body.appendChild(popup); // Use document.body to select the body element
     popup.style.backdropFilter="blur(9px)"; // Use document.body to select the body element
-    popup.style.height = "50vh";
-    popup.style.width = "50vw";
+    popup.style.height = height;
+    popup.style.width = width;
     popup.style.backgroundColor = "var(--white)"; // Use backgroundColor instead of backgroundcolor
     popup.style.borderRadius = "var(--border-radius)"; // Use backgroundColor instead of backgroundcolor
     popup.style.position = "fixed";
-    popup.style.top = "25%";
-    popup.style.left = "25%";
+    popup.style.top = top;
+    popup.style.left = left;
     popup.style.zIndex = "10";
 }
 
