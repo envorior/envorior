8081d88c6025f01842faffe3229267be3c1055e5
Muskan Maurya <muskan.maurya31@gmail.com>
2024-03-02 20:08:52 +0530

    backend

---
+++ envorior/generalapp/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render,redirect,reverse
+from .models import User,Profile
+
+# Create your views here.
+def registration(request):
+    if request.method=='POST':
+        email=request.POST['email']
+        password=request.POST['password']
+        contact_no=request.POST['contact_no']
+        full_name=request.POST['full_name']
+        dob=request.POST['dob']
+        state=request.POST['state']
+        city=request.POST['city']
+        pin_code=request.POST['pin_code']
+        try:
+            check_email = User.objects.get(email=email)
+            if check_email:
+                print('Already registered')
+                msg='Email already registered'
+                return render(request,'registration.html',locals())
+        except: 
+                user=User(email=email,password=password)
+                user.save()
+                profile=Profile(contact_no=contact_no,full_name=full_name,dob=dob,city=city,state=state,pin_code=pin_code)
+                profile.save()
+                return render(request,'login.html')
+    return render(request, 'registration.html')
+
+def login(request):
+    if request.method=='POST':
+        email=request.POST['email']
+        password=request.POST['password']
+        try:
+             user=User.objects.get(email=email)
+             if user.email==email and user.password==password and user.user_type=='general':
+                request.session['email'] = email
+                return redirect(reverse('publicapp:publichome'))
+             elif user.email==email and user.password==password and user.user_type=='servent':
+                return redirect('serventapp:dashboardparent')
+             elif user.email==email and user.password==password and user.user_type=='supervisor':
+                return redirect('serventapp:dashboardparent')
+        except:
+            msg ='Incorrect Email ID or Password'
+            return render(request,'login.html',locals())        
+    return render(request, 'login.html')
+
---
+++ envorior/publicapp/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from generalapp.models import Profile,User
+
+# Create your views here.
+def publichome(request):
+    email=request.session['email']
+    user = User.objects.get(email=email)
+    profile = Profile.objects.filter(user__email=email)
+    return render(request,'publichome.html',locals())
+
+def about(request):
+    return render(request,'about.html')
+def contact(request):
+    return render(request,'contact.html')
+def donation(request):
+    return render(request,'donation.html')
+def jobs(request):
+    return render(request,'jobs.html')
+def policy(request):
+    return render(request,'policy.html')
+def scam(request):
+    return render(request,'scam.html')
+def terms(request):
+    return render(request,'terms.html')
+def profile(request):
+    return render(request,'profile.html')
+def complain(request):
+    return render(request,'complain.html')
+def followers(request):
+    return render(request,'followers.html')
+def followings(request):
+    return render(request,'followings.html')
+def notifications(request):
+    return render(request,'notifications.html')
