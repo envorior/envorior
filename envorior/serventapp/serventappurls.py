68b6efa7a0ff4dbe0da77f21f2547d7f2e056259
anand gupta <anandgupta965193@gmail.com>
2024-03-20 21:01:19 +0530

    servent app continue

--- envorior/serventapp/serventappurls.py
+++ envorior/serventapp/serventappurls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from .import views
 urlpatterns = [
-    path('servent-dashboard/',views.serventparent,name='serventparent'),
+
     path('servent/',views.servent,name='servent'),
-    path('servent-post/',views.post,name='post'),
+    path('servent-complain/',views.complain,name='complain'),
     path('servent-event/',views.event,name='event'),
-    path('servent-serventprofile/',views.serventprofile,name='serventprofile'),
+    path('servent-dashboard/',views.dashboard,name='dashboard'),
     path('servent-notification/',views.notification,name='notification'),
+    path('servent-post/',views.post,name='post'),
     path('servent-rank/',views.rank,name='rank'),
+    path('servent-serventprofile/',views.serventprofile,name='serventprofile'),
 ]
 
--- envorior/serventapp/views.py
+++ envorior/serventapp/views.py
@@ -1,23 +1,26 @@
 from django.shortcuts import render
 
-def serventparent(request):
-    return render(request,'serventparent.html')
+def complain(request):
+    return render(request,'servent/pages/complain.html')
+
+def dashboard(request):
+    return render(request,'servent/pages/dashboard.html')
 
 def servent(request):
-    return render(request,'servent.html')
+    return render(request,'servent/pages/servent.html')
 
 def post(request):
-    return render(request,'post.html')
+    return render(request,'servent/pages/post.html')
 
 def event(request):
-    return render(request,'event.html')
+    return render(request,'servent/pages/event.html')
 
 def serventprofile(request):
-    return render(request,'serventprofile.html')
+    return render(request,'servent/pages/serventprofile.html')
 
 def notification(request):
-    return render(request,'notification.html')
+    return render(request,'servent/pages/notification.html')
 
 def rank(request):
-    return render(request,'rank.html')
+    return render(request,'servent/pages/rank.html')
 
